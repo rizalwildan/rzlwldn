747a22b43428e9f93731f4a6a93316d80d7e4087
rizal <rizal.wildan@tlab.co.id>
2022-03-29 17:03:48 +0700

    change about with quotes

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,13 @@
 import * as React from 'react'
-import {Link} from "gatsby";
 
-export default function About() {
+export default function About({ quote }) {
     return(
-        <section className="mb-16 lg:mb-64 md:mb-32">
-            <span className="text-4xl font-bold lg:text-5xl md:text-5xl">Hi.</span>
-            <p className="text-lg mt-4 lg:text-2xl md:text-2xl">
-                I'm LekoArts, your theme creator. I'm passionate about open source & teaching. Learn more about Gatsby & React on
-                <Link to="#" className="text-secondary hover:border-b hover:border-b-secondary ml-2">my website.</Link>
+        <section className="mb-16 lg:mb-64 md:mb-32 lg:px-8">
+            {/* <span className="text-4xl font-bold lg:text-5xl md:text-5xl">Hi.</span> */}
+            <p className="text-3xl mt-4 lg:text-5xl md:text-2xl text-left">
+                {quote.content}
             </p>
+            <p className='mt-4 text-lg lg:text-4xl md:text-4xl italic'>― {quote.author}</p>
         </section>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,17 @@ import {graphql, Link} from "gatsby";
 import {getSimplifiedPosts} from "../utils/helpers";
 import PostLists from "../components/postLists";
 
-export default function Home({ data }) {
+export default function Home({ data, serverData }) {
+  console.log(serverData)
   const latest = data.latest.edges
+  const quote = serverData.quote
   const simplifiedLatest = useMemo(() => getSimplifiedPosts(latest), [latest])
 
 
   return(
       <Layout>
           <Seo title="Homepage"/>
-          <About />
+          <About quote={quote}/>
           <section>
               <div className="flex justify-between border-b items-center pb-4 mb-8">
                   <h2 className="text-xl lg:text-3xl md:text-3xl">Latest Articles</h2>
@@ -50,3 +52,20 @@ export const pageQuery = graphql`
           }
     }
 `
+export const getServerData = async() => {
+  try {
+    const res = await fetch(`https://api.quotable.io/random?tags=famous-quotes&maxLength=100`)
+
+    return {
+      props: {
+        quote: await res.json()
+      }
+    }
+
+  } catch(error) {
+    return {
+      status: 500,
+      props: {}
+    }
+  }
+}
